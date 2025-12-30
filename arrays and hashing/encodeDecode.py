ca7ada34291b965cd28d4da2772e903b5fb36616
Balint Adolf <dolfi117@proton.me>
2025-12-30 17:04:59 +0200

    encode decode

---
+++ arrays and hashing/encodeDecode.py
@@ -0,0 +1,23 @@
+# Design an algorithm to encode a list of strings to a string. 
+# The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+def encode(self, strs):
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+def decode(self, s):
+    res = []
+    i = 0
+
+    while i < len(s):
+        j = i
+        while s[j] != '#':
+            j += 1
+        length = int(s[i:j])
+        i = j + 1
+        j = i + length
+        res.append(s[i:j])
+        i = j
+    return res
